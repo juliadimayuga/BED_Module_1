374358a2a867023b43859fbdea5ce50b936820b8
juliadimayuga <jdimayuga3@academic.rrc.ca>
2025-09-14 16:13:03 -0500

    Corrected switch cases.

--- src/portfolio/portfolioPerformance.ts
+++ src/portfolio/portfolioPerformance.ts
@@ -9,8 +9,6 @@ interface PortfolioPerformance {
         performanceSummary: string;
 }
 
-type Performance = "high" | "low";
-
 /**
  * Calculates the portfolio performance
  * @param initialInvestment - The amount initially invested
@@ -24,14 +22,28 @@ export function calculatePortfolioPerformance(
     const profitOrLoss: number = currentValue - initialInvestment;
     const percentageChange: number = (profitOrLoss / initialInvestment) * 100;
 
-    const performanceType: Performance = percentageChange > 20 ? "high" : "low";
-    let performanceSummary: string;
-    switch (performanceType){
-        case "high":
+    let performanceSummary: string = "";
+    switch (true){
+        case percentageChange > 20:
             performanceSummary = `The portfolio has gained significantly with a profit of $${profitOrLoss.toFixed(2)}.`;
             break;
-        case "low":
-            performanceSummary = `The portfolio has performed poorly.`;
+        case percentageChange > 10:
+            performanceSummary = `The portfolio has gained moderately with a profit of $${profitOrLoss.toFixed(2)}.`;
+            break;
+        case percentageChange > 0.1:
+            performanceSummary = `The portfolio has gained slightly with a profit of $${profitOrLoss.toFixed(2)}.`;
+            break;
+        case percentageChange > 0:
+            performanceSummary = `The portfolio has no change.`;
+            break;
+        case percentageChange > -0.1:
+            performanceSummary = `The portfolio has lost slightly with a loss of $${Math.abs(profitOrLoss).toFixed(2)}.`;
+            break;
+        case percentageChange > -10:
+            performanceSummary = `The portfolio has lost moderately with a loss of $${Math.abs(profitOrLoss).toFixed(2)}.`;
+            break;
+        case percentageChange > -20:
+            performanceSummary = `The portfolio has lost significantly with a loss of $${Math.abs(profitOrLoss).toFixed(2)}.`;
             break;
     }
 
